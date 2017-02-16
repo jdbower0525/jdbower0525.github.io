f14f5afee08248735870cb4b01174ccd7aea079d
John D. Bower <jdbower0525@gmail.com>
2017-02-16 14:50:17 -0500

    working on modal buttons

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-var modal = document.getElementById('myModal');
-var btn = document.getElementById("myBtn");
+var modal1 = document.getElementById('myModal1');
+var modal2 = document.getElementById('myModal2');
+var modal3 = document.getElementById('myModal3');
+var modal4 = document.getElementById('myModal4');
+var btn1 = document.getElementById("myBtn1");
+var btn2 = document.getElementById("myBtn2");
+var btn3 = document.getElementById("myBtn3");
+var btn4 = document.getElementById("myBtn4");
 var span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function() {
-    modal.style.display = "block";
+btn1.onclick = function() {
+    modal1.style.display = "block";
+}
+btn2.onclick = function() {
+    modal2.style.display = "block";
+}
+btn3.onclick = function() {
+    modal3.style.display = "block";
+}
+btn4.onclick = function() {
+    modal4.style.display = "block";
 }
 span.onclick = function() {
     modal.style.display = "none";
