a7d32e35814608c4d1090007019aac5d8cb31911
John D. Bower <jdbower0525@gmail.com>
2017-02-16 14:52:12 -0500

    working on modal buttons

--- index.js
+++ index.js
@@ -25,6 +25,6 @@ span.onclick = function() {
 }
 window.onclick = function(event) {
     if (event.target == modal) {
-        modal.style.display = "none";
+        modal1.style.display = "none";
     }
 }
